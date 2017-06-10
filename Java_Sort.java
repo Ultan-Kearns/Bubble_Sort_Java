f9cdfffe66f8dd79d6ba975506615dae1af6514f
Ultan <ultankearns@gmail.com>
2017-06-10 18:49:41 +0100

    Simple Sort in Java

---
+++ Java_Sort.java
@@ -0,0 +1,24 @@
+public class Java_Sort
+{
+	public static void main(String[] args)
+	{
+		int[] array = {1,8,6,3,2,0,4,56,4,6,565,5,655,65,56,56,5,655,65,2,22,2,23,5,4,6,56,8,9,1};
+
+		for(int i = array.length - 1; i > 0; i--)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				if(array[j] > array[i])
+				{
+					int temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+  	}
+		for(int i = 0; i < array.length; i++)
+		{
+			System.out.print(array[i] + " ");
+		}
+	}
+}
